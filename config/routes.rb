d816d11515e5883c9792a6d24d87774f57428c51
vasanth-krrish <vasanthkrrish96@gmail.com>
2020-09-17 17:39:27 +0530

    Added Navigation Bar and Favicon

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  root 'chatroom#index'
+  get '/login' => 'sessions#new', as: 'new_session'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
